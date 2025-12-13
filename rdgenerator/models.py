8772c858ddea7db6da9d4b28842eaa88e7d31f32
857074609 <73793536+857074609@users.noreply.github.com>
2025-12-13 23:17:41 +0800

    Update models.py

--- rdgenerator/models.py
+++ rdgenerator/models.py
@@ -4,5 +4,5 @@ class GithubRun(models.Model):
     id = models.IntegerField(verbose_name="ID",primary_key=True)
     uuid = models.CharField(verbose_name="uuid", max_length=100)
     status = models.CharField(verbose_name="status", max_length=100)
-    filename = models.CharField(max_length=100)
+    filename = models.CharField(max_length=255, default='rustdesk')
     direction = models.CharField(max_length=100,default='both') 
