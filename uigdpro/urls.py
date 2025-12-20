6cd7d6accab7cd371f8ab306cc1b7013946a4452
857074609 <73793536+857074609@users.noreply.github.com>
2025-12-20 16:42:57 +0800

    Update urls.py

--- uigdpro/urls.py
+++ uigdpro/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
 
     path('', views.generator_view, name='generator'),
 ）
-    path('check/', views.check_for_file, name='check_for_file'),
+    path('check', views.check_for_file, name='check_for_file'),
 
-    path('download/', views.download, name='download'),
+    path('download', views.download, name='download'),
 
-    path('png/', views.get_png, name='get_png'),
+    path('png', views.get_png, name='get_png'),
 
-    path('update_gh_run/', views.update_github_run, name='update_gh_run'),
+    path('update_gh_run', views.update_github_run, name='update_gh_run'),
 
-    path('save_custom_client/', views.save_custom_client, name='save_custom_client'),
+    path('save_custom_client', views.save_custom_client, name='save_custom_client'),
 
-    path('startgh/', views.startgh, name='startgh'),
+    path('startgh', views.startgh, name='startgh'),
 ]
