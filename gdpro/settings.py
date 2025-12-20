8baa7f99b2ed2e9ccbab8a815cc61833dd5c7170
857074609 <73793536+857074609@users.noreply.github.com>
2025-12-20 20:26:07 +0800

    Update settings.py

--- gdpro/settings.py
+++ gdpro/settings.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+GH_UPLOAD_TOKEN = os.getenv('GH_UPLOAD_TOKEN')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
