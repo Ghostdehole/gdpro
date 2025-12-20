4e1db1c71fad7ba0f1c58521980936846a96c3c3
857074609 <73793536+857074609@users.noreply.github.com>
2025-12-20 16:54:34 +0800

    Update settings.py

--- gdpro/settings.py
+++ gdpro/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-APPEND_SLASH = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 #if not SECRET_KEY:
