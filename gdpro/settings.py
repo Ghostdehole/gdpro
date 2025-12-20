fedbc55d84948d652e43bfca13686a7c06180677
857074609 <73793536+857074609@users.noreply.github.com>
2025-12-20 10:50:12 +0800

    Update settings.py

--- gdpro/settings.py
+++ gdpro/settings.py
@@ -33,7 +33,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
